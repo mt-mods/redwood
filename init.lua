774cf269d28ac8a0676b75857658e7f0a0c1fd08
OgelGames <olliverdc28@gmail.com>
2021-01-14 22:01:47 +1100

    use mod.conf, optional moreblocks

--- init.lua
+++ init.lua
@@ -7,8 +7,11 @@ dofile(MP.."/schematic.lua")
 dofile(MP.."/sapling.lua")
 dofile(MP.."/wood.lua")
 dofile(MP.."/leaves.lua")
-dofile(MP.."/stairs.lua")
 dofile(MP.."/mapgen.lua")
+
+if minetest.get_modpath("moreblocks") then
+	dofile(MP.."/stairs.lua")
+end
 
 if minetest.get_modpath("bonemeal") then
   dofile(MP.."/bonemeal.lua")
